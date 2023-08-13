c7600fb358dd10b13ced05f6c6644115a6e6d7f9
Milena <116603940+milenacurtinhas@users.noreply.github.com>
2023-08-13 16:39:22 -0300

    Create main.c

---
+++ deque/main.c
@@ -0,0 +1,64 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "deque.h"
+
+typedef struct
+{
+    int x, y;
+} Celula;
+
+Celula *celula_create(int x, int y)
+{
+    Celula *c = malloc(sizeof(Celula));
+    c->x = x;
+    c->y = y;
+    return c;
+}
+
+void celula_free(Celula *c)
+{
+    free(c);
+}
+
+int main()
+{
+    int i, n, x, y;
+    char cmd[10];
+    Deque *d = deque_create();
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("\n%s", cmd);
+
+        if (!strcmp(cmd, "PUSH_BACK"))
+        {
+            scanf("%d %d", &x, &y);
+            void* celula = celula_create(x,y);
+            deque_push_back(d, celula);
+        }
+        else if (!strcmp(cmd, "PUSH_FRONT"))
+        {
+            scanf("%d %d", &x, &y);
+            void* celula = celula_create(x,y);
+            deque_push_front(d, celula);
+        }
+        else if (!strcmp(cmd, "POP_BACK"))
+        {
+            Celula *c = deque_pop_back(d);
+            printf("%d %d\n", c->x, c->y);
+            celula_free(c);
+        }
+        else if (!strcmp(cmd, "POP_FRONT"))
+        {
+            Celula *c = deque_pop_front(d);
+            printf("%d %d\n", c->x, c->y);
+            celula_free(c);
+        }
+    }
+
+    deque_destroy(d);
+    return 0;
+}
