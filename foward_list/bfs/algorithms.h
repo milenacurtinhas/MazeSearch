ed17bbb5d435ebdc1e4a6b5c99f5eaacd255cb7f
Milena <116603940+milenacurtinhas@users.noreply.github.com>
2023-08-13 16:51:11 -0300

    Add files via upload

---
+++ foward_list/bfs/algorithms.h
@@ -0,0 +1,27 @@
+
+#ifndef _ALGORITHMS_H_
+#define _ALGORITHMS_H_
+
+#include "labirinto.h"
+
+typedef struct{
+    int x;
+    int y;
+    void *prev;
+    double dist_from_prev;
+} Celula;
+
+typedef struct
+{
+    Celula *caminho;
+    double custo_caminho;
+    int tamanho_caminho;
+    int nos_expandidos;
+    int sucesso;
+} ResultData;
+
+void expand_maze(Celula* c, Labirinto* l, Deque* d);
+int valid_position(int x, int y, Labirinto* l);
+ResultData breadth_first_search(Labirinto *l, Celula inicio, Celula fim);
+
+#endif
